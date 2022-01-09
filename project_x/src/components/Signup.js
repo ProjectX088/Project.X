67d3e5009a6deb20d1a86b9c15c314307ba1d371
gosce4550 <92060664+gosce4550@users.noreply.github.com>
2022-01-08 16:23:37 -0800

    Creating Sign in Page

---
+++ project_x/src/components/Signup.js
@@ -0,0 +1,47 @@
+import React, { useRef }from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
+import { useAuth } from './contexts/AuthContext'
+export default function Signup(){
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+
+    function handleSubmit(event){
+        event.prevenDefault()
+
+        signup(emailRef.current.value, passwordRef.current.value)
+    }
+    return(
+        <>
+            <Card>
+                <Card.Body>
+                    <h1 className="text-center mb-4">Sign Up</h1>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" style={{padding: "10"}} placeholder="projectX@something.com" ref={emailRef} required/>
+                        </Form.Group>
+                        
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" ref={passwordRef} required/>
+                        </Form.Group>
+
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm Password" ref={passwordConfirmRef} required/>
+                        </Form.Group>
+                        
+                        <Button className="w-100" type="submit">Submit</Button>
+                        
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log in
+            </div>
+        </>
+    )
+};
